9ae4cc123ae5dd0034447ad2f8870fe25f717511
dhruvkumar200 <123928947+dhruvkumar200@users.noreply.github.com>
2023-02-09 18:49:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.btn1').on('click', function() {
+    $('.modelbox').show();
+    $('.close').on('click',function(){
+      $('.modelbox').hide();
+
+    })
+
+});
+   
+  });
